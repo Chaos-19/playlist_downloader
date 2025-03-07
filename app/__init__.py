8f23c5d659c472c375ff1f0a310a09c4c3187b10
Chaos-19 <kalgetachew375@gmail.com>
2025-03-07 16:33:16 +0300

    fix error in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ load_dotenv()
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        backend=os.getenv('CELERY_RESULT_BACKEND').,
+        backend=os.getenv('CELERY_RESULT_BACKEND'),
         broker=os.getenv('CELERY_BROKER_URL')
     )
     celery.conf.update(app.config)

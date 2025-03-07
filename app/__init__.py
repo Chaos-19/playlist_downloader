5bb0c321575551cc1dc703139f56d78d1c0db9c5
Chaos-19 <kalgetachew375@gmail.com>
2025-03-07 12:56:01 +0300

    change to hard code the redis url

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ import logging
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        backend=os.environ.get('REDIS_URL'),
-        broker=os.environ.get('REDIS_URL')
+        backend="redis://redis:6379/0",
+        broker="redis://redis:6379/0"
     )
     celery.conf.update(app.config)
     return celery

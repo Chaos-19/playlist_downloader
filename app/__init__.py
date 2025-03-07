cee0ef1701d7943f87f9947165626c33d4edf056
Chaos-19 <kalgetachew375@gmail.com>
2025-03-07 11:47:35 +0300

    update Dockerfile & docker-composer.yaml

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from celery import Celery
+import os
 
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        backend="redis://localhost:6379/0",
-        broker="redis://localhost:6379/0"
+        backend=os.environ.get('REDIS_URL'),
+        broker=os.environ.get('REDIS_URL')
     )
-    celery.conf.update(app.config)
+    celery.conf.update(app.config)h
     return celery
 
 # Initialize Flask
@@ -23,34 +24,3 @@ from app.blueprints.yt_downloader import api_ytvd
     
 app.register_blueprint(main)
 app.register_blueprint(api_ytvd,url_prefix="/api")
-
-'''
-from flask import Flask
-
-from celery import Celery
-
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL']
-    )
-    celery.conf.update(app.config)
-    return celery
-
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config')
-
-    from app.routes import main
-    from app.blueprints.yt_downloader import api_ytvd
-    
-    app.register_blueprint(main)
-    app.register_blueprint(api_ytvd,url_prefix="/api")
-    
-    celery = make_celery(app)
-
-    return app
-'''
